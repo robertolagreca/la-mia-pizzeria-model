44e13a5385afc9f01a0ac8b609904635357543eb
robertolagreca <robertolagreca92@gmail.com>
2023-01-09 17:38:44 +0100

    Esercizio finito (unico problema su url immagini che sto risolvendo).

--- LaMiaPizzeria1/Utils/PizzaData.cs
+++ LaMiaPizzeria1/Utils/PizzaData.cs
@@ -15,19 +15,19 @@ namespace LaMiaPizzeriaModel.Utils
                 return pizzas;
             }
 
-            pizzas = new List<Pizza>();
+             pizzas = new List<Pizza>();
+            
+              Pizza pizza1 = new Pizza(1, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Margherita", "Sugo, mozzarella", 4.99f);
+              Pizza pizza2 = new Pizza(2, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Wusterl", "Sugo, mozzarella, wusterl", 5.99f);
+              Pizza pizza3 = new Pizza(3, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Americana", "Sugo, mozzarella, wusterl, patatine", 6.99f);
+              Pizza pizza4 = new Pizza(4, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Salamino", "Sugo, mozzarella, salamino piccante", 5.99f); 
 
-            Pizza pizza1 = new Pizza(1, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Margherita", "Sugo, mozzarella", 4.99f);
-            Pizza pizza2 = new Pizza(2, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Wusterl", "Sugo, mozzarella, wusterl", 5.99f);
-            Pizza pizza3 = new Pizza(3, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Americana", "Sugo, mozzarella, wusterl, patatine", 6.99f);
-            Pizza pizza4 = new Pizza(4, "https://it.wikipedia.org/wiki/File:Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Salamino", "Sugo, mozzarella, salamino piccante", 5.99f);
-
-            pizzas.Add(pizza1);
-            pizzas.Add(pizza2);
-            pizzas.Add(pizza3);
-            pizzas.Add(pizza4);
-
-            return pizzas;
+              pizzas.Add(pizza1);
+              pizzas.Add(pizza2);
+              pizzas.Add(pizza3);
+              pizzas.Add(pizza4);
+            
+            return pizzas; 
 
         }
     }
